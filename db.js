6725da5ac0b79acea66ba324ccc37aac4b7e94fc
BoostedPenguin <alex20009@abv.bg>
2021-06-02 18:10:53 +0200

    Using environmental variables

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const admin = require('firebase-admin');
 
 // Configure firebase
-var serviceAccount = require("./connfy_firebase_cred.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.applicationDefault()
 });
+
+
 
 const db = admin.firestore();
 
