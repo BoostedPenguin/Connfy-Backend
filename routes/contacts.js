7142c2c071235c03dfac55101f86a24fddc451e8
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2021-06-07 09:50:56 +0200

    remove contact

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,23 +2,6 @@ var express = require('express');
 const db = require('../db');
 var router = express.Router();
 
-//returns an array of contacts userUID for a specific user
-router.get('/', async (req, res, next) => {
-    const userUID = "asdjawdjawdg"
-
-    var contacts = [];
-
-    const contactsRef = db.collection('users_testing').doc(userUID).collection('contacts');
-    const snapshot = await contactsRef.get();
-
-    snapshot.forEach(doc => {
-        console.log(doc.id, '=>', doc.data());
-        contacts.push(doc.id);
-    });
-
-    res.send(contacts);
-});
-
 //adds contacts to userID
 router.get('/add', async (req, res, next) => {
     const userUID = "asdjawdjawdg"
@@ -37,6 +20,33 @@ router.get('/add', async (req, res, next) => {
     });
 
     res.send("done");
+});
+
+//returns an array of contacts userUID for a specific user
+router.get('/', async (req, res, next) => {
+    const userUID = "asdjawdjawdg"
+
+    var contacts = [];
+
+    const contactsRef = db.collection('users_testing').doc(userUID).collection('contacts');
+    const snapshot = await contactsRef.get();
+
+    snapshot.forEach(doc => {
+        console.log(doc.id, '=>', doc.data());
+        contacts.push(doc.id);
+    });
+
+    res.send(contacts);
+});
+
+//deletes contact from user
+router.get('/delete', async (req, res, next) => {
+    const userUID = "asdjawdjawdg"
+    const user2UID = "rgasdasdzas"
+
+    await db.collection('users_testing').doc(userUID).collection('contacts').doc(user2UID).delete();
+
+    res.send("deleted");
 });
 
 module.exports = router;
