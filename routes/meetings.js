493fd80bca71b23149629f3f23a98cc5639b7347
krishum77 <balccbf@gmail.com>
2021-06-09 15:38:07 +0200

    code formatting

--- routes/meetings.js
+++ routes/meetings.js
@@ -1,18 +1,27 @@
-var express  = require('express')
+let express  = require('express')
 const db = require('../db')
-var router = express.Router()
-var admin  = require('firebase-admin')
-var checkIfAuthenticated = require('../middlewares/authentication_middleware');
+let router = express.Router()
+let admin  = require('firebase-admin')
+let checkIfAuthenticated = require('../middlewares/authentication_middleware');
 
 //Get All Meetings
 router.get('/', checkIfAuthenticated, async (req, res, next) => {
-    const query = db.collection('users').doc(req.authId);
-    const querySnapshot = await query.get();
+    const allUserMeetingsRef = db.collection('users').doc(req.authId);
+    const allUserMeetingsSnapshot = await allUserMeetingsRef.get();
 
-    if(querySnapshot.exists){
-        res.send(querySnapshot.data());
+    if(allUserMeetingsSnapshot.exists){
+        let allMeetings = [];
+        for (const meeting of allUserMeetingsSnapshot.data().meetings) {
+            const snapshot = await db.collection('meetings').doc(meeting).get();
+            const meetingData = {
+                id: snapshot.id,
+                details: snapshot.data()
+            };
+            allMeetings.push(meetingData);
+        }
+        res.send(allMeetings);
     }else{
-        res.send('No Meetings Found for current user')
+        res.sendStatus(204);
     }
 });
 
@@ -22,21 +31,21 @@ router.get('/:id', checkIfAuthenticated, async (req, res, next) => {
     const querySnapshot = await query.get();
 
     if(querySnapshot.exists){
-            let result_id = querySnapshot.id
+            let result_id = querySnapshot.id;
             let result = querySnapshot.data();
 
         const data = {
                     id: result_id,
                     meetingData: result
-                }
+                };
 
          if(data.meetingData.invitedUsers.includes(req.authId) || data.meetingData.ownerUID === req.authId){
-            res.send(data)
+            res.send(data);
          }else{
-            res.send('Not Authorized to access this meeting')
+            res.sendStatus(403);
         }
     }else{
-            res.send('No Result Found')
+            res.sendStatus(404);
         }
 });
 
@@ -45,20 +54,20 @@ router.post('/add', checkIfAuthenticated, async (req, res, next) => {
     const data = {
         meetingDetails: {
             title : req.body.title,
-            ownerUID : req.body.ownerUID,
+            ownerUID : req.authId,
             ownerName : req.body.ownerName,
             date : admin.firestore.FieldValue.serverTimestamp(),
             geoLocation : req.body.geoLocation,
             invitedUsers: req.body.invitedUsers
         }
-    }
+    };
     const meetingsQuerySnapshot = await db.collection('meetings').add(data.meetingDetails);
     await db.collection('users').doc(data.meetingDetails.ownerUID).update({meetings: admin.firestore.FieldValue.arrayUnion(meetingsQuerySnapshot.id)});
 
     for (const user of data.meetingDetails.invitedUsers) {
-        await db.collection('users').doc(user).update({meetings: admin.firestore.FieldValue.arrayUnion(meetingsQuerySnapshot.id)})
+        await db.collection('users').doc(user).update({meetings: admin.firestore.FieldValue.arrayUnion(meetingsQuerySnapshot.id)});
     }
-    res.send("Created New Meeting")
+    res.send("Created New Meeting");
 });
 
 //Update a Meeting
@@ -74,30 +83,30 @@ router.put('/update/:id', checkIfAuthenticated, async (req, res, next) =>{
             const data = {
                 meetingDetails: {
                     title : req.body.title,
-                    ownerUID : req.body.ownerUID,
                     ownerName : req.body.ownerName,
+                    ownerUID: req.authId,
                     date : admin.firestore.FieldValue.serverTimestamp(),
                     geoLocation : req.body.geoLocation,
                     invitedUsers: req.body.invitedUsers
                 }
-            }
-            await db.collection('meetings').doc(req.params.id).set(data.meetingDetails)
+            };
+            await db.collection('meetings').doc(req.params.id).set(data.meetingDetails);
 
             for (const user of invitedUsers) {
                 if(!data.meetingDetails.invitedUsers.includes(user)){
-                    await db.collection('users').doc(user).update({meetings: admin.firestore.FieldValue.arrayRemove(req.params.id)})
+                    await db.collection('users').doc(user).update({meetings: admin.firestore.FieldValue.arrayRemove(req.params.id)});
                 }
             }
 
             for (const user of data.meetingDetails.invitedUsers) {
-                await db.collection('users').doc(user).update({meetings: admin.firestore.FieldValue.arrayUnion(req.params.id)})
+                await db.collection('users').doc(user).update({meetings: admin.firestore.FieldValue.arrayUnion(req.params.id)});
             }
-            res.send('Meeting Updated!')
+            res.send('Meeting Updated!');
         }else{
-            res.send('Not Authorized to access this meeting')
+            res.sendStatus(403);
         }
     }else{
-        res.sendStatus(404)
+        res.sendStatus(404);
     }
 });
 
@@ -107,15 +116,15 @@ router.delete('/delete/:id', checkIfAuthenticated, async (req, res, next) =>{
     const querySnapshot = await query.get();
 
     if(querySnapshot.exists){
-        let invitedUsers = querySnapshot.data().invitedUsers
-        invitedUsers.push(querySnapshot.data().ownerUID)
+        let invitedUsers = querySnapshot.data().invitedUsers;
+        invitedUsers.push(req.authId);
         for (const user of invitedUsers) {
-                await db.collection('users').doc(user).update({meetings: admin.firestore.FieldValue.arrayRemove(req.params.id)})
+                await db.collection('users').doc(user).update({meetings: admin.firestore.FieldValue.arrayRemove(req.params.id)});
         }
-        await query.delete()
-        res.send('Meeting Deleted!')
+         await query.delete();
+        res.send('Meeting Deleted!');
     }else{
-        res.sendStatus(404)
+        res.sendStatus(404);
     }
 
 })
