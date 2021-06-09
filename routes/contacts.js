4c10b0b7dd7c00558c026a81408b963f3c320985
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2021-06-09 15:34:09 +0200

    error handling

--- routes/contacts.js
+++ routes/contacts.js
@@ -9,33 +9,42 @@ router.post('/add', checkIfAuthenticated, async (req, res) => {
     const userId = req.authId;
     const arrayUnion = admin.firestore.FieldValue.arrayUnion;
 
-    await db.collection('users').doc(userId).update({
-        contacts: arrayUnion(req.body[0])
-    });
-
-    res.send("done");
+    try {
+        await db.collection('users').doc(userId).update({
+            contacts: arrayUnion(req.body[0])
+        });
+        res.send("done");
+    } catch (e) {
+        res.status(404).send(e);
+    };
 });
 
 //returns an array of a user's contacts
 router.get('/', checkIfAuthenticated, async (req, res) => {
     const userId = req.authId;
     const contacts = (await db.collection('users').doc(userId).get()).data().contacts;
-    if (contacts.length > 0)
-        res.send(contacts);
-    else
-        res.status(204).send();
+    try {
+        if (contacts.length > 0)
+            res.send(contacts);
+        else
+            res.status(204).send();
+    } catch (e) {
+        res.status(404).send(e);
+    };
 });
 
 //deletes contact from user
 router.post('/delete', checkIfAuthenticated, async (req, res) => {
     const userId = req.authId;
     const arrayRemove = admin.firestore.FieldValue.arrayRemove;
-
-    await db.collection('users').doc(userId).update({
-        contacts: arrayRemove(req.body[0])
-    });
-
-    res.send("deleted");
+    try {
+        await db.collection('users').doc(userId).update({
+            contacts: arrayRemove(req.body[0])
+        });
+        res.send("deleted");
+    } catch (e) {
+        res.status(404).send(e);
+    };
 });
 
 module.exports = router;
