81e1e7f78014ec2663e16704b4c1111b0295cb0a
BoostedPenguin <alex20009@abv.bg>
2021-06-10 14:05:07 +0200

    Add person to database on app registration

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var exampleRouter = require('./routes/example');
 var contactsRouter = require('./routes/contacts')
+var usersRouter = require('./routes/users')
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/example', exampleRouter);
 app.use('/contacts', contactsRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
